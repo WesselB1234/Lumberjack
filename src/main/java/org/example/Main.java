1140be8da26c99a2616c1c2338b20511ffedba9f
Mark de Haan <mark@starwave.nl>
2025-08-29 11:51:51 +0200

    Updated for 2025

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,35 +2,37 @@ package org.example;
 
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFrame;
 
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
-        Terminal terminal = null;
+        DefaultTerminalFactory factory = new DefaultTerminalFactory();
 
-        try {
-            terminal = defaultTerminalFactory.createTerminal();
+        try (Terminal terminal = factory.createTerminal()) {
+
+            // Event handling in case of a Swing terminal frame,
+            // so the application closes properly when the user clicks the close button of the window.
+            if (terminal instanceof SwingTerminalFrame) {
+                SwingTerminalFrame swing = (SwingTerminalFrame) terminal;
+                swing.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                swing.addWindowListener(new WindowAdapter() {
+                    @Override public void windowClosing(WindowEvent e) { System.exit(0); // if you want to be extra sure
+                    }
+                });
+            }
+
             Game game = new Game(terminal);
             game.start();
-
-            terminal.readInput();
         }
         catch(IOException e) {
             e.printStackTrace();
-        }
-        finally {
-            if(terminal != null) {
-                try {
-                    terminal.close();
-                }
-                catch(IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }
